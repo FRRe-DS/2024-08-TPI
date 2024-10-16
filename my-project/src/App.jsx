3005192ac3a59fb71911b712680e6dd72c5917ab
Matias Bangher <matiasbangher06@gmail,com>
2024-10-16 14:52:58 -0300

    merge??

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,6 +3,42 @@ import Header from './views/components/Header';
 import Footer from './views/components/Footer';
 import Cuerpo from './views/components/Cuerpo';
 import Eventos from './views/components/Eventos';
+import Escultores from './views/components/Escultores'; 
+import Esculturas from './views/components/Esculturas';
+import Create from './views/components/Create'; // Asegúrate de importar el componente Create
+
+const App = () => {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen"> 
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Cuerpo />} />
+            <Route path="/eventos" element={<Eventos />} />
+            <Route path="/escultores" element={<Escultores />} />
+            <Route path="/esculturas" element={<Esculturas />} />
+            
+            {/* Ruta para CRUD */}
+            <Route path="/crud" element={<Create />} />
+            
+            {/* Página 404 */}
+            <Route path="*" element={<div>404 Not Found</div>} />
+          
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './views/components/Header';
+import Footer from './views/components/Footer';
+import Cuerpo from './views/components/Cuerpo';
+import Eventos from './views/components/Eventos';
 import Escultores from './views/components/Escultores'; // Importa otros componentes según sea necesario
 import Esculturas from './views/components/Esculturas';
 import Votacion from './views/components/Votacion';
