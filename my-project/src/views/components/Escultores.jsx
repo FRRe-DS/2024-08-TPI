f98406f14eabd0c8b450fd70e4478b50eff5ba90
miguet22 <joamigue22@gmail.com>
2024-10-17 16:23:14 -0300

    cambios tailwind

--- my-project/src/views/components/Escultores.jsx
+++ my-project/src/views/components/Escultores.jsx
@@ -19,17 +19,17 @@ const Escultores = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-5 gap-4 p-10">
       {escultores.map((escultor) => (
-        <div key={escultor.id} className="relative bg-white shadow-lg p-3 rounded-lg max-w-xs">
+        <div key={escultor.id} className="relative shadow-lg p-3 rounded-lg max-w-xs bg-customGray">
           {/* Bandera o imagen de la nacionalidad */}
           <img
-            className="absolute top-1 left-4 w-10 h-6"
+            className="absolute top-1 left-4 w-10 h-6 "
             src={escultor.img_nacionalidad} // Acceso correcto al campo de imagen de la nacionalidad
             alt={`Bandera de ${escultor.nacionalidad}`} // Alt text con la nacionalidad
           />
 
           {/* Imagen de perfil del escultor */}
           <img
-            className="w-24 h-24 mx-auto mb-5"
+            className="w-24 h-24 mx-auto mb-5 "
             src={escultor.imagen_esc} // Imagen del escultor
             alt={escultor.nombre_esc} // Alt text con el nombre del escultor
           />
@@ -37,7 +37,7 @@ const Escultores = () => {
           {/* Información del escultor */}
           <div className="text-center">
             <h2 className="text-xl font-semibold">{escultor.nombre_esc}</h2>
-            <p className="text-gray-500">{escultor.nacionalidad}</p>
+            <p className="text-grisOscuro">{escultor.nacionalidad}</p>
           </div>
         </div>
       ))}
--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customGray: '#9fa3a9', // Define el color personalizado
+        grisOscuro: '#393d42', // Color gris oscuro personalizado
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
