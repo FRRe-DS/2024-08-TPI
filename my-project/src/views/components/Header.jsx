c5dde6fbad5d6dfd82627aa42fc45f5a10b70cb7
AgusCabralC <agustinrobertino@gmail.com>
2024-10-26 17:53:28 -0300

    panel de control actualizado

--- my-project/src/views/components/Header.jsx
+++ my-project/src/views/components/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import '/public/css/Header.css'; 
 import { useAuth0 } from '@auth0/auth0-react';
+import { Dropdown } from 'flowbite-react'; // Importa el dropdown de Flowbite
 
 
 const Header = () => {
@@ -58,67 +59,40 @@ const Header = () => {
         
     return (
         <header className="header">
-            <div className="logo">
-                <Link to="/"><img src="img/logo.png" alt="Logo de la marca" /></Link>
-            </div>
-            <nav>
-                <ul className="nav-links">
-                    <li><Link to="/escultores">Escultores</Link></li>
-                    <li><Link to="/esculturas">Esculturas</Link></li>
-                    <li><Link to="/eventos">Eventos</Link></li>
-                    {isAuthenticated && userHasRole('user') && (<li><Link to="/votacion">Votación</Link></li>)}
-                </ul>
-            </nav>
+    <div className="logo">
+        <Link to="/"><img src="img/logo.png" alt="Logo de la marca" /></Link>
+    </div>
+    <nav>
+        <ul className="nav-links">
+            <li><Link to="/escultores">Escultores</Link></li>
+            <li><Link to="/esculturas">Esculturas</Link></li>
+            <li><Link to="/eventos">Eventos</Link></li>
+            {isAuthenticated && userHasRole('user') && (<li><Link to="/votacion">Votación</Link></li>)}
+        </ul>
+    </nav>
 
-            {!isAuthenticated ? (
-                <button className="btn" onClick={loginWithRedirect}>Iniciar Sesión</button>
-            ) : (
-                <div className="user-info" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <img 
-                    src={user.picture || "img/avatar.png"} 
-                    alt="User Icon" 
-                    onClick={toggleLogout}  
-                    style={{ cursor: 'pointer', borderRadius: '50%', width: '50px', height: '50px', margin: '0 10px' }} // Espaciado entre el logo y los botones
-                />
-                {showLogout && (
-                    <div className="logout-menu" style={{ display: 'flex', alignItems: 'center' }}>
-                        <button class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-                            onClick={() => logout({ returnTo: window.location.origin })}   
-                        >
-                            Cerrar Sesión
-                        </button>
-                        {userHasRole("admin") && (
-                            <Link to="/admin" style={{ marginLeft: '10px' }}>
-                                <button class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                                    Panel de Control
-                                </button>
-                            </Link>
-                        )}
-                    </div>
-                )}
-                </div>
-
-
+    {!isAuthenticated ? (
+        <button className="btn" onClick={loginWithRedirect}>Iniciar Sesión</button>
+    ) : ( 
+        <Dropdown
+        label={<img src={user.picture || "img/avatar.png"} alt="User Icon" style={{ borderRadius: '50%', width: '50px', height: '50px', cursor: 'pointer' }} />}>
+            <Dropdown.Item onClick={() => logout({ returnTo: window.location.origin })}>
+            <Link to="/admin" className="text-black">
+                        Cerrar Sesión
+                    </Link>
+            </Dropdown.Item>
+            {userHasRole("admin") && (
+                <Dropdown.Item>
+                    <Link to="/admin" className="text-black">
+                        Panel de Control
+                    </Link>
+                </Dropdown.Item>
             )}
+        </Dropdown>
+    )}
+</header>
 
-            <a onClick={openNav} className="menu"><button>Menu</button></a>
 
-            {isOpen && (
-                <div id="mobile-menu" className={`overlay ${isOpen ? 'open' : ''}`}>
-                <button onClick={closeNav} aria-label="Cerrar menú" className="close">&times;</button>
-                         <div className="overlay-content">
-                        <Link to="/escultores">Escultores</Link>
-                        <Link to="/esculturas">Esculturas</Link>
-                        <Link to="/eventos">Eventos</Link>
-                        {isAuthenticated && userHasRole() ? (
-                            <Link to="/votacion">Votación</Link>
-                        ) : (
-                            <a onClick={loginWithRedirect}>Iniciar Sesión</a>
-                        )}
-                    </div>
-                </div>
-            )}
-        </header>
     );
 };
 
